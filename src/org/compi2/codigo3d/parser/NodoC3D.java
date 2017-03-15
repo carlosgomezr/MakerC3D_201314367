e269f539c9da69c0d57656eb64931827326c9d39
carlosgomezr <carlos001909@gmail.com>
2017-03-15 17:43:33 -0600

    HT2
    Hoja de Trabajo 2 Arreglada

--- src/org/compi2/codigo3d/parser/NodoC3D.java
+++ src/org/compi2/codigo3d/parser/NodoC3D.java
@@ -8,13 +8,32 @@ package org.compi2.codigo3d.parser;
  */
 public class NodoC3D {
     private String cad;
-
+    public String ETQF;
+    public String ETQV;
+   
     public NodoC3D(String cad) {
         this.cad = cad;
     }
     
+    public NodoC3D(String v, String f) {
+        this.ETQV = v;
+        this.ETQF = f;
+    }
+    
     public String getCad(){
         return cad;
+    }
+    public String getFalsa(){
+        return ETQF;
+    }
+    public String getVerdadera(){
+        return ETQV;
+    }
+    public void setFalsa(String etqf){
+        this.ETQF = etqf;
+    }
+    public void setVerdadera(String etqv){
+        this.ETQV = etqv;
     }
         
 }
